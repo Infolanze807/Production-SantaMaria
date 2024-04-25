4e15efe86f8b3fa36ad8b873023367213f8f265a
infolanze <infolanze.tech@gmail.com>
2024-04-25 10:56:33 +0530

    Second

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -109,6 +109,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
 Sidenav.defaultProps = {
   brandImg: "/img/logo-ct.png",
   brandName: "Santa Maria",
+  sidenavColor: "dark",
 };
 
 Sidenav.propTypes = {
