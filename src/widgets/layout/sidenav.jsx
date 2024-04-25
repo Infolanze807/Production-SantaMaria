3b9c541a6111109d5b4cedeb80eab4936a886a64
infolanze <infolanze.tech@gmail.com>
2024-04-25 11:44:09 +0530

    First

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -30,7 +30,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
 
   return (
     <aside
-      className={`${sidenavTypes[sidenavType]} ${
+      className={`bg-gradient-to-br from-gray-800 to-gray-900 ${
         openSidenav ? "translate-x-0" : "-translate-x-80"
       } fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0 border border-blue-gray-100`}
     >
